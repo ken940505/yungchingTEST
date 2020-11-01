10372109b3652b8e917baa7715bb31e0d70c2aca
許斯惟 <68268715+ken940505@users.noreply.github.com>
2020-11-01 23:53:34 +0800

    加入專案檔案。

---
+++ yungchingTEST/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using yungchingTEST.Models;
+
+namespace yungchingTEST.Controllers
+{
+    public class HomeController : Controller
+    {
+        yungEntities db = new yungEntities(); //建立資料庫
+
+        // GET: Home
+        public ActionResult Home()
+        {
+            var lists = from p in db.Yung
+                        select p; 
+
+            return View(lists); //載入資料
+        }
+    }
+}
